a3d81102076b86b8387febbabaae2134df32286c
Manoj <manojromina1997@gmail.com>
2017-04-26 00:09:04 +0530

    Added Google analtics

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -39,6 +39,20 @@ addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); func
 <script src="parsley.min.js"></script>
 <!--Form Validation -->
 
+
+<!--Analytics-->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-98057883-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<!--Analytics-->
+
 </head>
 <body>
 <!--Headers-->
